9a7f2a1b82926bbdc3958fb5c1bb118b60cf4600
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-02-02 22:42:24 +0100

    Add function to show team at  user login/registration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Team;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $teams = Team::all();
+
+        return view('pages.home', compact('teams'));
     }
+    
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,52 +1,23 @@
 @extends('layouts.main-layout')
 @section('content')
     @auth 
-        <h1>ciao {{Auth::user() -> name}}</h1>
-        <a href="{{route('logout')}}">Logout</a>
+        <h1>Teams </h1>
+        <ul>
+            @foreach ($teams as $team)
+                <li>
+                    <a href="">{{$team -> team_name}}</a>
+                    - {{$team -> sport}}
+                </li>
+
+            @endforeach
+        </ul>
+
+        
     @else
 
-        <h3>Ti devi loggare</h3>
+        <h3>Ciao per visualizzare i nostri contenuti Registrati o effettua il Login</h3>
         
     @endauth
     
-    <div>
-        <h2>Register</h2>
-        <form action="{{route('register')}}" method="POST">
-            @method("POST")
-            @csrf
-
-            <label for="name">Nome</label>
-            <input type="text" name="name"> <br>
-            <label for="email">Email</label>
-            <input type="text" name="email"> <br>
-            <label for="password">Password</label>
-            <input type="password" name="password"> <br>
-            <label for="password_confirmation">Password Confirm</label>
-            <input type="password" name="password_confirmation"> <br>
-            <input type="submit" value="Registrati">
-            
-        </form>
-
-    </div>
-    <div>
-        <h2>Login</h2>
-        <form action="{{route('login')}}" method="POST">
-            @method("POST")
-            @csrf
-
-            
-            <label for="email">Email</label>
-            <input type="text" name="email"> <br>
-            <label for="password">Password</label>
-            <input type="password" name="password"> <br>
-            
-            <input type="submit" value="Login">
-            
-        </form>
-
-    </div>
-
-
-
-
+    
 @endsection
